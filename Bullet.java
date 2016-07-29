19f27c73bd382bc9f215ef132766677ee22b028b
HoAngAnh3686 <hoang.levan.95@gmail.com>
2016-07-29 19:47:21 +0700

    Add files via upload

---
+++ Bullet.java
@@ -0,0 +1,14 @@
+package models;
+
+/**
+ * Created by GGG8 on 7/27/2016.
+ */
+public class Bullet {
+    public int x;
+    public int y;
+    public void moveTo(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+}
